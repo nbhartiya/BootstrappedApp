cc92edbae45b07a9b839a436954b5b23e75f2994
nbhartiya <neeharika@alum.mit.edu>
2013-01-26 11:32:59 -0800

    added controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bootstrap::Application.config.secret_token = '4b96c825b7aaae6f2c25542abab28b437ea802fd5a62689dd956bbd6c4a1a3ee4dbd03bbafd59bf5f5e1c7fe995ef861848e9a65b9e225522e06d4f872d6308b'
